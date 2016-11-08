2dee635ad0ceb07b919df8b77dbac6952fd8b692
Glenn Jahnke <gleenn@gmail.com>
2016-11-08 00:04:16 -0800

    WIP merge sorted lists

--- src/sort/core.clj
+++ src/sort/core.clj
@@ -11,7 +11,7 @@
         mut-xs-j (get mut-xs j)]
     (if (comparator mut-xs-i mut-xs-j)
       (do
-        (println (str "[" (string/join " " mut-xs) "] swapping " mut-xs-i " and " mut-xs-j))
+;;         (println (str "[" (string/join " " mut-xs) "] swapping " mut-xs-i " and " mut-xs-j))
         (aset mut-xs i mut-xs-j)
         (aset mut-xs j mut-xs-i)))))
 
@@ -20,6 +20,18 @@
     (do
       (doseq [i (range (inc (count xs)))]
         (doseq [j (reverse (range 1 i))]
-          (println 'i i 'j j)
+;;           (println 'i i 'j j)
           (swap-if mut-xs j (dec j) <)))
       (into [] mut-xs))))
+
+(defn merge-sorted [xs ys & sorted]
+  (let [sorted (or sorted (list)) _ (prn xs ys sorted)]
+    (cond
+      (empty? xs) (concat sorted ys)
+      (empty? ys) (concat sorted xs)
+      :else (let [x (first xs) y (first ys)]
+              (if (< x y)
+                (merge-sorted (drop 1 xs) ys (concat (list x) sorted))
+                (merge-sorted (drop 1 ys) xs (concat (list y) sorted))
+                )))))
+
--- test/sort/core_midje.clj
+++ test/sort/core_midje.clj
@@ -2,9 +2,14 @@
   (:require [midje.sweet :refer :all]
             [sort.core :as subject]))
 
-(fact "flags can be parsed from an arglist"
+(fact "sorts lists using bubble sort"
       :has-metadata
       (subject/bubble [])     => []
       (subject/bubble [0])     => [0]
       (subject/bubble [3 1 2]) => [1 2 3]
       (subject/bubble [3 1 2 0]) => [0 1 2 3])
+
+(fact "merges two sorted lists"
+      :has-metadata
+      (subject/merge-sorted [] []) => []
+      (subject/merge-sorted [1 3 5] [2 4 6]) => [1 2 3 4 5 6])
