561f5815d286bb36ba9d5a82675fdb6795b56d89
Glenn Jahnke <gleenn@gmail.com>
2016-11-14 21:43:19 -0800

    merge sort done

--- src/sort/merge.clj
+++ src/sort/merge.clj
@@ -1,6 +1,6 @@
 (ns sort.merge)
 
-(defn merge-sorted-fast [xs ys]
+(defn merge-sorted [xs ys]
   (reverse
     (loop [xs xs ys ys sorted '()]
       (cond
@@ -10,3 +10,12 @@
                 (if (< x y)
                   (recur (drop 1 xs) ys (conj sorted x))
                   (recur (drop 1 ys) xs (conj sorted y))))))))
+
+(defn merge-sort [xs]
+  (let [n (count xs)]
+    (if (< n 2)
+      xs
+      (let [split (quot n 2)]
+        (merge-sorted
+          (merge-sort (take split xs))
+          (merge-sort (drop split xs)))))))
--- test/sort/merge_midje.clj
+++ test/sort/merge_midje.clj
@@ -4,9 +4,16 @@
 
 
 (fact "merges two sorted lists with less recursion bro"
-      (subject/merge-sorted-fast [] []) => []
-      (subject/merge-sorted-fast [1] []) => [1]
-      (subject/merge-sorted-fast [1 2] []) => [1 2]
-      (subject/merge-sorted-fast [1 1 1] []) => [1 1 1]
-      (subject/merge-sorted-fast [1 3 5] [2 4 6]) => '(1 2 3 4 5 6)
-      (subject/merge-sorted-fast (range 10) '()) => (range 10))
+      (subject/merge-sorted [] []) => []
+      (subject/merge-sorted [1] []) => [1]
+      (subject/merge-sorted [1 2] []) => [1 2]
+      (subject/merge-sorted [1 1 1] []) => [1 1 1]
+      (subject/merge-sorted [1 3 5] [2 4 6]) => '(1 2 3 4 5 6)
+      (subject/merge-sorted (range 10) '()) => (range 10))
+
+(fact "sorts lists"
+      (subject/merge-sort []) => []
+      (subject/merge-sort [1]) => [1]
+      (subject/merge-sort [3 2 1]) => [1 2 3]
+      (subject/merge-sort [3 2 1 4]) => [1 2 3 4]
+      (subject/merge-sort (shuffle (range 10))) => (range 10))
