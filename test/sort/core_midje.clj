66e15381f702efbcd256d8625b2684ca5ebb6d11
Glenn Jahnke <gleenn@gmail.com>
2016-11-14 20:27:54 -0800

    add non-recursive version of merger for super speed

--- test/sort/core_midje.clj
+++ test/sort/core_midje.clj
@@ -17,3 +17,13 @@
       (subject/merge-sorted [1 1 1] []) => [1 1 1]
       (subject/merge-sorted [1 3 5] [2 4 6]) => '(1 2 3 4 5 6)
       (subject/merge-sorted (range 10) '()) => (range 10))
+
+(fact "merges two sorted lists with less recursion bro"
+      :has-metadata
+      (subject/merge-sorted-fast [] []) => []
+      (subject/merge-sorted-fast [1] []) => [1]
+      (subject/merge-sorted-fast [1 2] []) => [1 2]
+      (subject/merge-sorted-fast [1 1 1] []) => [1 1 1]
+      (subject/merge-sorted-fast [1 3 5] [2 4 6]) => '(1 2 3 4 5 6)
+      (subject/merge-sorted-fast (range 10) '()) => (range 10))
+
