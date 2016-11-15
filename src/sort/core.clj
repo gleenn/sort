dbe59791ab082872f7de15e6cba9b87ae77ab362
Glenn Jahnke <gleenn@gmail.com>
2016-11-14 20:10:41 -0800

    cleanup using defn-

--- src/sort/core.clj
+++ src/sort/core.clj
@@ -25,15 +25,15 @@
       (into [] mut-xs))))
 
 
-(defn merge-sorted [xs ys]
-  (let [merge-sorted-helper (fn merge-sorted-helper [xs ys sorted]
-                              (cond
-                                (empty? xs) (concat (reverse ys) sorted)
-                                (empty? ys) (concat (reverse xs) sorted)
-                                :else (let [x (first xs) y (first ys)]
-                                        (if (< x y)
-                                          (merge-sorted-helper (drop 1 xs) ys (conj sorted x))
-                                          (merge-sorted-helper (drop 1 ys) xs (conj sorted y))
-                                          ))))]
-    (reverse (merge-sorted-helper (lazy-seq xs) (lazy-seq ys) '()))))
+(defn- merge-sorted- [xs ys sorted]
+  (cond
+    (empty? xs) (concat (reverse ys) sorted)
+    (empty? ys) (concat (reverse xs) sorted)
+    :else (let [x (first xs) y (first ys)]
+            (if (< x y)
+              (merge-sorted- (drop 1 xs) ys (conj sorted x))
+              (merge-sorted- (drop 1 ys) xs (conj sorted y))
+              ))))
 
+(defn merge-sorted [xs ys]
+    (reverse (merge-sorted- (lazy-seq xs) (lazy-seq ys) '())))
